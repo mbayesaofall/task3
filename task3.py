130f41ec3b69f9bdb5267c1fdfdd1c2cacc6bca2
Mbaye Fall <msfall@memphis.edu>
2025-05-26 19:39:23 -0500

    condition added

--- task3.py
+++ task3.py
@@ -1,4 +1,11 @@
 hour_worked = int(input("Enter the hour worked : "))
 pay_rate = float(input("Enter pay rate : "))
-gross_pay = hour_worked * pay_rate
-print(f"The gross pay is : {gross_pay:.2f}")
+if hour_worked <=  40 : 
+    gross_pay = hour_worked * pay_rate
+    print(f"The gross pay is : {gross_pay:.2f}")
+else:
+    extra_hour = hour_worked - 40
+    overtime_pay = extra_hour * 1.56 * pay_rate
+    gross_pay = overtime_pay + ( 40 * pay_rate)
+    print(f"overtime pay : {overtime_pay}")
+    print("The gross pay is : {gross_pay}")
